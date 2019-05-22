1d99fd928c44b267a4aebcb5c74b663014fd09f7
Denis Volkov <DenisVolkov67@gmail.com>
2019-05-22 11:02:49 +0300

    Update Models. Аdd projects for logic, data

--- EntertainmentPortal/EP.Sudoku.Logic/Models/GameBoard.cs
+++ EntertainmentPortal/EP.Sudoku.Logic/Models/GameBoard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EP.Sudoku.Web.Models
+namespace EP.Sudoku.Logic.Models
 {
     public class GameBoard
     {
--- EntertainmentPortal/EP.Sudoku.Logic/Models/Session.cs
+++ EntertainmentPortal/EP.Sudoku.Logic/Models/Session.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EP.Sudoku.Logic.Enums;
 
-namespace EP.Sudoku.Web.Models
+namespace EP.Sudoku.Logic.Models
 {
     public class Session
     {
         public int Id { get; set; }
-        public Player Player { get; set; } // is it necessary?
-        public Enums.DifficultyLevel Level { get; set; } 
+        public Player Player { get; set; }
+        public DifficultyLevel Level { get; set; }
         public bool IsOver { get; set; }
+        /// <summary>
+        /// conserns players' ExperiencePoints 
+        /// </summary>
         public double Duration { get; set; }
     }
 }
