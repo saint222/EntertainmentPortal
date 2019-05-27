a2887b6371acdc2d7d8aa6d0f3008b60e69730ca
Pavel <pavel561@gmail.com>
2019-05-27 18:45:12 +0300

    Edit Word model
    add constructor

--- EntertainmentPortal/EP.WordsMaker/EP.WordsMaker.Logic/Models/Word.cs
+++ EntertainmentPortal/EP.WordsMaker/EP.WordsMaker.Logic/Models/Word.cs
@@ -5,10 +5,39 @@ using System.Threading.Tasks;
 
 namespace EP.WordsMaker.Logic.Models
 {
-    public class Word
-    {
-        public string _word;
+	public class Word
+	{
+		public int MyProperty { get; set; }
+		public char[] Letters
+		{
+			get { return _letters; }
+		}
 
-        public char[] _letters; 
-    }
+        private string _word;
+		private char[] _letters;
+		private Dictionary<char, int> _wordDict;
+
+		public Word(string word)
+		{
+			this._word = word;
+			this._letters = word.ToArray();
+			_wordDict = new Dictionary<char, int>();
+			foreach(char letter in _letters)
+			{
+				char _char = letter;
+				if(_wordDict.ContainsKey(_char))
+				{
+					_wordDict[_char]++;
+				}
+				else
+				{
+					_wordDict.Add(_char,1);
+				}
+			}
+		}
+		public int GetCharCount(char Char)
+		{
+			return 0;
+		}
+	}
 }
