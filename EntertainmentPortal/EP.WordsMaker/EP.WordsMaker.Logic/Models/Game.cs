6ab88a16085e79bc23386ffbf5b23aa61024a861
Pavel <pavel561@gmail.com>
2019-05-29 16:35:52 +0300

    add project WordsMaker.Tests

--- EntertainmentPortal/EP.WordsMaker/EP.WordsMaker.Logic/Models/Game.cs
+++ EntertainmentPortal/EP.WordsMaker/EP.WordsMaker.Logic/Models/Game.cs
@@ -5,17 +5,56 @@ using System.Threading.Tasks;
 
 namespace EP.WordsMaker.Logic.Models
 {
-    public class Game
+	///<summary>
+	///
+	///</summary>
+	///<remarks>
+	///
+	/// </remarks>
+	public class Game
     {
-        //Time _time;  
-        
-        public string KeyWord { get; set; }
+		private const int PLAYER_COUNT = 2;
+		private Rules _rules;
+		private Dictionary<Player, List<Word>> _words;
+		private List<Player> _players;
 
-        public Rules _rules;
+		public string KeyWord { get; set; }
 
-        public Dictionary<Word, Player> _words;
+		public Rules Rules
+		{
+			get { return _rules; }
+		}
+		public List<Player> Players
+		{
+			get { return _players; }
+		}
 
-        public List<Player> _players;
+		public Game()
+		{
+			this._players = new List<Player>();
+			this._rules = new Rules();
+			this._words = new Dictionary<Player, List<Word>>();
+		}
+		public void AddPlayer(Player player)
+		{
+			if(this._players.Count < PLAYER_COUNT)
+			{
+				this._players.Add(player);
+				_words.Add(player, new List<Word>());
+			}
+				
+		}
+		public void SetRules(Rules rules)
+		{
+			this._rules = rules;
+		}
+		public void ChangePlayer()
+		{
 
+		}
+		public void InsertWord(Player player, string word)
+		{
+
+		}
     }
 }
