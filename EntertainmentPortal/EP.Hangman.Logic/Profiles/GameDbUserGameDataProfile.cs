4a50321cd958c5ac762ed203d66bbad78007f09f
Sergey Makarov <sgmak@me.com>
2019-06-06 01:26:43 +0300

    Delete commented

--- EntertainmentPortal/EP.Hangman.Logic/Profiles/GameDbUserGameDataProfile.cs
+++ EntertainmentPortal/EP.Hangman.Logic/Profiles/GameDbUserGameDataProfile.cs
@@ -15,16 +15,11 @@ namespace EP.Hangman.Logic.Profiles
             CreateMap<string, List<string>>().ConvertUsing(new ListStringTypeConverter());
             CreateMap<List<string>, string>().ConvertUsing(new StringTypeConverter());
             CreateMap<GameDb, UserGameData>()
-                //.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                //.ForMember(dest => dest.PickedWord, opt => opt.MapFrom(src => src.PickedWord))
-                .ForMember(dest => dest.CorrectLetters,
-                    opt => opt.MapFrom(src => src.CorrectLetters))
-                //.ForMember(dest => dest.UserErrors, opt => opt.MapFrom(src => src.UserErrors))
+                .ForMember(dest => dest.CorrectLetters, opt => opt.MapFrom(src => src.CorrectLetters))
                 .ForMember(dest => dest.Alphabet, opt => opt.MapFrom(src => src.Alphabet))
                 .ReverseMap()
                 .ForMember(dest => dest.Alphabet, opt => opt.MapFrom(src => src.Alphabet))
                 .ForMember(dest => dest.CorrectLetters, opt => opt.MapFrom(src => src.CorrectLetters));
-
         }
     }
 }
