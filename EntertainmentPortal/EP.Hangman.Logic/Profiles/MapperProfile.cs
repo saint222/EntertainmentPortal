2c1d2f82935ef7b03e72cab2fe2ec17b60cd9a5d
Sergiro Z <zujonok@hotmail.com>
2019-06-11 20:28:03 +0300

    Revert "up to origin"
    This reverts commit e89ad01d0720bc98f1c9fdea3770f70f28b0cd1f.

---
+++ EntertainmentPortal/EP.Hangman.Logic/Profiles/MapperProfile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using EP.Hangman.Data.Models;
+using EP.Hangman.Logic.Models;
+using EP.Hangman.Logic.Converters;
+
+namespace EP.Hangman.Logic.Profiles
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<UserGameData, ControllerData>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.UserErrors, opt => opt.MapFrom(src => src.UserErrors))
+                .ForMember(dest => dest.Alphabet, opt => opt.MapFrom(src => src.Alphabet))
+                .ForMember(dest => dest.CorrectLetters, opt => opt.MapFrom(src => src.CorrectLetters))
+                .ReverseMap();
+            CreateMap<string, List<string>>().ConvertUsing(new ListStringTypeConverter());
+            CreateMap<List<string>, string>().ConvertUsing(new StringTypeConverter());
+            CreateMap<GameDb, UserGameData>()
+                .ForMember(dest => dest.CorrectLetters, opt => opt.MapFrom(src => src.CorrectLetters))
+                .ForMember(dest => dest.Alphabet, opt => opt.MapFrom(src => src.Alphabet))
+                .ForMember(dest => dest.UserErrors, opt => opt.MapFrom(src => src.UserErrors))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.PickedWord, opt => opt.MapFrom(src => src.PickedWord))
+                .ReverseMap()
+                .ForMember(dest => dest.Alphabet, opt => opt.MapFrom(src => src.Alphabet))
+                .ForMember(dest => dest.CorrectLetters, opt => opt.MapFrom(src => src.CorrectLetters))
+                .ForMember(dest => dest.UserErrors, opt => opt.MapFrom(src => src.UserErrors))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.PickedWord, opt => opt.MapFrom(src => src.PickedWord));
+
+        }
+    }
+}
