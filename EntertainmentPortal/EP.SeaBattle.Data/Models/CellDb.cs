2b49d96c769a1121060cfdff9ee1a311cb4f7473
Sechik <sechalexur@gmail.com>
2019-06-12 14:53:43 +0300

    add logic for ship field

--- EntertainmentPortal/EP.SeaBattle.Data/Models/CellDb.cs
+++ EntertainmentPortal/EP.SeaBattle.Data/Models/CellDb.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using EP.SeaBattle.Common.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EP.SeaBattle.Data.Models
 {
-    public class PlayerDb
+    public class CellDb
     {
-
         //TODO Change to Guid
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        public string NickName { get; set; }
+        public byte X { get; set; }
 
-        public bool CanShoot { get; set; }
+        public byte Y { get; set; }
 
+        public CellStatus Status { get; set; }
     }
 }
