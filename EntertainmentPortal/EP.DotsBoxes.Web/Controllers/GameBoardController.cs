0fcd06bce2c415dc64771aff69db498629efc824
Dmitry Yaruta <dima.yaruta@gmail.com>
2019-06-18 20:08:21 +0300

    add PUT

--- EntertainmentPortal/EP.DotsBoxes.Web/Controllers/GameBoardController.cs
+++ EntertainmentPortal/EP.DotsBoxes.Web/Controllers/GameBoardController.cs
@@ -4,9 +4,11 @@ using System.Threading.Tasks;
 using EP.DotsBoxes.Logic.Commands;
 using EP.DotsBoxes.Logic.Models;
 using EP.DotsBoxes.Logic.Queries;
+using JetBrains.Annotations;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
 using NSwag.Annotations;
 
 namespace EP.DotsBoxes.Web.Controllers
@@ -15,35 +17,60 @@ namespace EP.DotsBoxes.Web.Controllers
     public class GameBoardController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<GameBoardController> _logger;
 
-        public GameBoardController(IMediator mediator)
+        public GameBoardController(IMediator mediator, ILogger<GameBoardController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         // GET api/gameboard
         [HttpGet("api/gameboard")]
-        [SwaggerResponse(HttpStatusCode.OK, typeof(IEnumerable<GameBoard>), Description = "Received new game board")]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(IEnumerable<Cell>), Description = "Received game board")]
         [SwaggerResponse(HttpStatusCode.NotFound, typeof(void), Description = "Game board is not created")]
         public async Task<IActionResult> GetGameBoardAsync()
         {
+            _logger.LogDebug($"Action: {ControllerContext.ActionDescriptor.ActionName}");
             var result = await _mediator.Send(new GetGameBoard()).ConfigureAwait(false);
-            return result.Any() ? (IActionResult)Ok(result) : NotFound();
+            _logger.LogWarning($"Exit from method: {ControllerContext.ActionDescriptor.ActionName}");
+
+            return result.HasValue ? (IActionResult)Ok(result.Value) : NotFound();
         }
 
         // POST api/gameboard
         [HttpPost("api/gameboard")]
         [SwaggerResponse(HttpStatusCode.OK, typeof(GameBoard), Description = "Create new game board")]
         [SwaggerResponse(HttpStatusCode.BadRequest, typeof(void), Description = "Invalid data")]
-        public async Task<IActionResult> PostNewGameBoardAsync([FromBody]GameBoard model)
+        public async Task<IActionResult> NewGameBoardAsync([FromBody][NotNull]NewGameBoardCommand model)
         {
-           var result = await _mediator.Send(new CreateNewGameBoardCommand
-            {
-                Rows = model.Rows,
-                Columns = model.Columns
-            });
+            _logger.LogDebug($"Action: {ControllerContext.ActionDescriptor.ActionName} Parameters: Game board: " +
+                $"Columns = {model.Columns}, Rows = {model.Rows}");
 
-            return Ok(result);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _mediator.Send(model);
+            _logger.LogWarning($"Exit from method: {ControllerContext.ActionDescriptor.ActionName}");
+
+            return result.IsSuccess ? (IActionResult)Ok(result.Value) : BadRequest(result.Error);
         }
+
+        //PUT api/gameboard
+       [HttpPut("api/gameboard")]
+       [SwaggerResponse(HttpStatusCode.OK, typeof(Cell), Description = "Update game board")]
+       [SwaggerResponse(HttpStatusCode.BadRequest, typeof(void), Description = "Invalid data")]
+        public async Task<IActionResult> UpdateGameBoardAsync([FromBody][NotNull]UpdateGameBoardCommand model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _mediator.Send(model);
+            return result.IsSuccess ? (IActionResult)Ok(result) : BadRequest(result.Error);
+        }
+
     }
 }
