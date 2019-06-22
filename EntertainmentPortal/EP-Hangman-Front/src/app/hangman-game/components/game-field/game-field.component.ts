aae8ef45312441a0be273d223aad9ec602fc68b2
Sergey Makarov <sgmakgg@gmail.com>
2019-06-22 17:35:04 +0300

    Add front structure of hangman (empty)
    +module
    +model
    +service
    +component

---
+++ EntertainmentPortal/EP-Hangman-Front/src/app/hangman-game/components/game-field/game-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-field',
+  templateUrl: './game-field.component.html',
+  styleUrls: ['./game-field.component.sass']
+})
+export class GameFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ EntertainmentPortal/EP-Hangman-Front/src/app/hangman-game/hangman-game.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameFieldComponent } from './components/game-field/game-field.component';
+
+@NgModule({
+  declarations: [GameFieldComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HangmanGameModule { }
