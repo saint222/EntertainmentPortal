96fda8a337bddb3f397a844e31e42e941f07c5bd
Sergey Makarov <sgmakgg@gmail.com>
2019-06-22 18:39:13 +0300

    Rename project

--- EntertainmentPortal/hangman-client/src/app/app.component.spec.ts
+++ EntertainmentPortal/hangman-client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'EP-Hangman-Front'`, () => {
+  it(`should have as title 'hangman-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('EP-Hangman-Front');
+    expect(app.title).toEqual('hangman-client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to EP-Hangman-Front!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hangman-client!');
   });
 });
