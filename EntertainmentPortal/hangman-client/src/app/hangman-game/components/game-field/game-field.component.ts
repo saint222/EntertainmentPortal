4e06174b9dc1f342b0e6d8c6b1a20a56f154fba8
Vsevolod <2007juggernaut@gmail.com>
2019-06-25 19:52:39 +0300

    Refactor front components
    - components/alphabet-buttons
    - components/start-game
    * components/game-field

--- EntertainmentPortal/hangman-client/src/app/hangman-game/components/game-field/game-field.component.ts
+++ EntertainmentPortal/hangman-client/src/app/hangman-game/components/game-field/game-field.component.ts
@@ -9,16 +9,34 @@ import { HttpResponseBase } from '@angular/common/http';
   styleUrls: ['./game-field.component.sass']
 })
 export class GameFieldComponent implements OnInit {
-  @Input() gameFieldData: GameData = null;
-  @Output() updatedGameData = new EventEmitter();
-
+  gameFieldData: GameData = null;
+  tmpGameData: GameData = null;
 
   constructor(private gameService: GameService) { }
 
   ngOnInit() {
   }
 
-  updateField(newData) {
-    this.updatedGameData.emit(newData);
+  checkLetter(letter: string) {
+    this.tmpGameData = new GameData();
+
+    this.tmpGameData.id = this.gameFieldData.id;
+    this.tmpGameData.letter = letter;
+    this.tmpGameData.alphabet = null;
+    this.tmpGameData.correctLetters = null;
+    this.tmpGameData.userErrors = 0;
+
+    this.gameService.updateGame(this.tmpGameData).subscribe(b => {
+      this.gameFieldData = b;
+    },
+        (err: HttpResponseBase) => console.log(err.statusText)
+    );
+  }
+
+  startNewGame() {
+    this.gameService.createGame().subscribe(b => {
+      this.gameFieldData = b;
+    },
+      (err: HttpResponseBase) => console.log(err.statusText));
   }
 }
--- EntertainmentPortal/hangman-client/src/app/hangman-game/hangman-game.module.ts
+++ EntertainmentPortal/hangman-client/src/app/hangman-game/hangman-game.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameFieldComponent } from './components/game-field/game-field.component';
 import {HttpClientModule} from '@angular/common/http';
-import { AlphabetButtonsComponent } from './components/alphabet-buttons/alphabet-buttons.component';
-import { StartGameComponent } from './components/start-game/start-game.component';
 
 @NgModule({
-  declarations: [GameFieldComponent, AlphabetButtonsComponent, StartGameComponent],
+  declarations: [GameFieldComponent],
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  exports: [StartGameComponent]
+  exports: [GameFieldComponent]
 })
 export class HangmanGameModule { }
