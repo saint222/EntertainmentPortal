215b25f7d2be29cc78c78df8b53fc6a10e96c5aa
Dmitry Yaruta <dima.yaruta@gmail.com>
2019-07-14 18:35:27 +0300

    Add cell controller

---
+++ EntertainmentPortal/EP.DotsBoxes.Logic/Handlers/GetCellsHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using CSharpFunctionalExtensions;
+using EP.DotsBoxes.Data.Context;
+using EP.DotsBoxes.Data.Models;
+using EP.DotsBoxes.Logic.Models;
+using EP.DotsBoxes.Logic.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EP.DotsBoxes.Logic.Handlers
+{
+    public class GetCellsHandler : IRequestHandler<GetCells, Maybe<IEnumerable<Cell>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly GameBoardDbContext _context;
+
+        public GetCellsHandler(IMapper mapper, GameBoardDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        
+        public async Task<Maybe<IEnumerable<Cell>>> Handle(GetCells request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Cells
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            IEnumerable<Cell> cells = _mapper.Map<List<CellDb>, IEnumerable<Cell>>(result);
+
+            return result.Any() ?
+                Maybe<IEnumerable<Cell>>.From(cells) :
+                Maybe<IEnumerable<Cell>>.None;
+        }
+    }
+}
---
+++ EntertainmentPortal/EP.DotsBoxes.Web/Controllers/CellController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using EP.DotsBoxes.Logic.Models;
+using EP.DotsBoxes.Logic.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
+
+namespace EP.DotsBoxes.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CellController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<CellController> _logger;
+
+        public CellController(IMediator mediator, ILogger<CellController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        // GET api/cell
+        [HttpGet("api/cell")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(IEnumerable<Cell>), Description = "Received cells")]
+        [SwaggerResponse(HttpStatusCode.NotFound, typeof(void), Description = "Cells not found")]
+        public async Task<IActionResult> GetCellsAsync()
+        {
+            _logger.LogDebug($"Action: {ControllerContext.ActionDescriptor.ActionName}");
+            var result = await _mediator.Send(new GetCells());
+            _logger.LogWarning($"Exit from method: {ControllerContext.ActionDescriptor.ActionName}");
+
+            return result.HasValue ? (IActionResult)Ok(result.Value) : NotFound();
+        }
+    }
+}
