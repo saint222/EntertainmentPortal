70a5a96e86b25e0127529006af09ea6e2e90715f
Sergey Makarov <sgmakgg@gmail.com>
2019-07-15 01:03:41 +0300

    Repair

--- EntertainmentPortal/EP.Hangman.Security/Config.cs
+++ EntertainmentPortal/EP.Hangman.Security/Config.cs
@@ -37,9 +37,9 @@ namespace EP.Hangman.Security
                     ClientName = "Client Credentials Client",
 
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = {new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256())},
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedScopes = {"api1"}
+                    AllowedScopes = { "api1" }
                 },
 
                 // MVC client using hybrid flow
@@ -49,14 +49,14 @@ namespace EP.Hangman.Security
                     ClientName = "MVC Client",
 
                     AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
-                    ClientSecrets = {new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256())},
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
-                    RedirectUris = {"http://localhost:5001/signin-oidc"},
+                    RedirectUris = { "http://localhost:5001/signin-oidc" },
                     FrontChannelLogoutUri = "http://localhost:5001/signout-oidc",
-                    PostLogoutRedirectUris = {"http://localhost:5001/signout-callback-oidc"},
+                    PostLogoutRedirectUris = { "http://localhost:5001/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
-                    AllowedScopes = {"openid", "profile", "api1"}
+                    AllowedScopes = { "openid", "profile", "api1" }
                 },
 
                 // SPA client using implicit flow
@@ -77,10 +77,10 @@ namespace EP.Hangman.Security
                         "http://localhost:5002/popup.html",
                     },
 
-                    PostLogoutRedirectUris = {"http://localhost:5002/index.html"},
-                    AllowedCorsOrigins = {"http://localhost:5002"},
+                    PostLogoutRedirectUris = { "http://localhost:5002/index.html" },
+                    AllowedCorsOrigins = { "http://localhost:5002" },
 
-                    AllowedScopes = {"openid", "profile", "api1"}
+                    AllowedScopes = { "openid", "profile", "api1" }
                 }
             };
         }
--- EntertainmentPortal/hangman-client/src/app/hangman-game/components/start-screen/start-screen.component.ts
+++ EntertainmentPortal/hangman-client/src/app/hangman-game/components/start-screen/start-screen.component.ts
@@ -1,6 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+
+  // Url of the Identity Provider
+  issuer: 'http://localhost:5000',
+
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin + '/startScreen',
+
+  // The SPA's id. The SPA is registerd with this id at the auth-server
+  clientId: 'spa',
+
+  // set the scope for the permissions the client should request
+  // The first three are defined by OIDC. The 4th is a usecase-specific one
+  scope: 'openid profile hangman_api',
+};
+
+
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
@@ -8,7 +30,11 @@ import { Router } from '@angular/router';
 })
 export class StartScreenComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: OAuthService) {
+    this.authService.configure(authConfig);
+    this.authService.tokenValidationHandler = new JwksValidationHandler();
+    this.authService.loadDiscoveryDocumentAndTryLogin();
+   }
 
   ngOnInit() {
   }
@@ -18,7 +44,8 @@ export class StartScreenComponent implements OnInit {
   }
 
   loginUser() {
-    this.router.navigateByUrl('/login');
+    // this.router.navigateByUrl('/login');
+    this.authService.initImplicitFlow();
   }
 
   registerUser() {
