d16cc915dabec522cdfe837cb842ec84dff1bad8
Yana Knysh <jemelachee@gmail.com>
2019-07-18 22:04:38 +0300

    WIP: Front add letter to cell

---
+++ EntertainmentPortal/EP.Balda/EP.Balda.Data/Transaction.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Text;
+
+namespace EP.Balda.Data
+{
+    class Transaction
+    {
+        public void Do()
+        {
+            var con = new SQLiteConnection(@"Data Source =..\EP.Balda.Data\DbStore\baldaGameDb.db;Version=3;New=True;Compress=True;");
+            
+            try
+            {
+                con.Open();
+                int counter = 0;
+                string line;
+
+                // Read the file and display it line by line.  
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(@"c:\test.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(line);
+                    counter++;
+                }
+
+                file.Close();
+                System.Console.WriteLine("There were {0} lines.", counter);
+                // Suspend the screen.  
+                System.Console.ReadLine();
+                string Query = "insert into sqliteDb(ID,Name) values('"+ "')";
+            
+                SQLiteCommand cmd = new SQLiteCommand(Query, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error");
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ EntertainmentPortal/EP.Balda/EP.Balda.Web/Services/Helpers.cs
@@ -0,0 +1,41 @@
+﻿using EP.Balda.Logic.Models;
+
+namespace EP.Balda.Web.Services
+{
+    public static class Helpers
+    {
+        public static Cell[,] Do2DimArray(Map map)
+        {
+            var cells = new Cell[map.Size, map.Size];
+
+            for (int i = 0; i < map.Size; i++)     // lines
+            {
+                for (int j = map.Size - 1; j >= 0; j--) // columns
+                {
+                    foreach (var c in map.Cells)
+                    {
+                        if (j == c.X && i == c.Y)
+                        {
+                            cells[i, j] = c;
+                        }
+                    }
+                }
+            }
+
+            return cells;
+        }
+
+        public static char[] GetEnglishAlphabet()
+        {
+            char[] alphabet = new char[26];
+            char letter = 'A';
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = letter++;
+            }
+
+            return alphabet;
+        }
+    }
+}
