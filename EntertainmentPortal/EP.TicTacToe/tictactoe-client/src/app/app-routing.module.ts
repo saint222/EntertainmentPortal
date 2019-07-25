2eaf8be9e4017129e4ae6721e5bf07a4d0d6d1b5
SergiroZ <zujonok@gmail.com>
2019-07-25 18:28:45 +0300

    WIP: front

--- EntertainmentPortal/EP.TicTacToe/tictactoe-client/src/app/app-routing.module.ts
+++ EntertainmentPortal/EP.TicTacToe/tictactoe-client/src/app/app-routing.module.ts
@@ -1,21 +1,18 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {MenuComponent} from './tictactoe-game/components/menu/menu.component';
 import {HomeComponent} from './tictactoe-game/components/home/home.component';
-// import {UserComponent} from './tictactoe-game/components/user/user.component';
+
 
 const routes: Routes = [
+
   {path: 'home', component: HomeComponent},
-  {path: 'menu', component: MenuComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: []
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  exports: [RouterModule]
 })
-
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
